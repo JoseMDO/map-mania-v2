f73bfbe656ea6aa6f97b6b80d6b1933e83b16fc6
JoseMDO <joseamontesdeocamo@lewisu.edu>
2023-10-31 23:20:54 -0500

    Got the correct location fucntion working

--- client/app.js
+++ client/app.js
@@ -18,28 +18,84 @@ async function initMap() {
       zoom: 4,
       center: {lat: 41.5250, lng: -88.0817},
       mapId: "DEMO_MAP_ID",
+      gestureHandling: "greedy"
     });
-  
-    function mapLocations(locationsArray) {
-        locationsArray.forEach((location) => {
-            const marker = new google.maps.Marker({
-                position: {lat:location.location.lat, lng:location.location.lng},
-                map: gMap,
-                title: location.title,
-                icon: location.iconImage,
-                content: location.content
-            })
-            const infoWindow = new google.maps.InfoWindow({
-                content: marker.content
-            });
-            marker.addListener('click', () => {
-                infoWindow.open({
-                    anchor: marker,
-                    map: gMap
-                })
-            })
-        });
+    
+
+    function isMarkerChoiceValid(marker, locationLat, locationLng) {
+        let valid = false; 
+        const locationLatMaxNorthernDifference = locationLat + 1
+        const locationLatMaxSoutherDifference = locationLat - 1
+        const locationLngMaxEasternDifference = locationLng + 1
+        const locationLngMaxWesternDifference = locationLng - 1
+
+        if (marker.getPosition().lat() > locationLatMaxNorthernDifference || marker.getPosition().lat() < locationLatMaxSoutherDifference
+            || marker.getPosition().lng() > locationLngMaxEasternDifference || marker.getPosition().lng() < locationLngMaxWesternDifference) {
+                valid = false
+            } else {
+                valid = true
+            }
+        return valid
     }
+ 
+    // function addMarker() {
+    //     google.maps.event.addListener(gMap, "click", (e) => {
+    //       const marker = new google.maps.Marker({
+    //         position: e.latLng,
+    //         map: gMap
+    //       });
+    //       setTimeout(() => {
+    //         marker.setMap(null);
+    //       }, 3000);
+    //     });
+    //   }
+
+  
+    function mapLocations(locationsArray) {      
+        let currentLocationIndex = 0
+        let locationFound = false
+        google.maps.event.addListener(gMap, "click", (e) => {
+            const marker = new google.maps.Marker({
+                position: e.latLng,
+                map: gMap
+            })
+            setTimeout(() => {
+                marker.setMap(null);
+            }, 3000);
+            console.log(locationsArray[currentLocationIndex].location.lat, locationsArray[currentLocationIndex].location.lng)
+            locationFound = isMarkerChoiceValid(marker, locationsArray[currentLocationIndex].location.lat, locationsArray[currentLocationIndex].location.lng)
+            if (locationFound) {
+                currentLocationIndex += 1
+                
+            } else {
+                console.log("keep trying")
+            }
+        })
+    }
+
+
+
+
+
+
+        
+        // const marker = new google.maps.Marker({
+        //     position: {lat:location.location.lat, lng:location.location.lng},
+        //     map: gMap,
+        //     title: location.title,
+        //     icon: location.iconImage,
+        //     content: location.content
+        // })
+        // const infoWindow = new google.maps.InfoWindow({
+        //     content: marker.content
+        // });
+        // marker.addListener('click', () => {
+        //     infoWindow.open({
+        //         anchor: marker,
+        //         map: gMap
+        //     })
+        // })
+
   
     google.maps.event.addListener(gMap, 'idle', function () {
         updateGame();
@@ -56,6 +112,7 @@ async function initMap() {
         console.log("inbounds:" + inBounds + " zoomLevel: "+zoomLevel);
     }
     mapLocations(locationsArray);
+    // addMarker();
   }
   
 initMap();
