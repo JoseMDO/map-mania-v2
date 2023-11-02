892dea796f1cadd65b5dedb5ab4ee9c5dcc2b784
JoseMDO <joseamontesdeocamo@lewisu.edu>
2023-11-02 00:51:20 -0500

    added full modal functionality and scoring

--- client/app.js
+++ client/app.js
@@ -1,8 +1,6 @@
 let gMap;
 let locationsArray;
-
-
-
+let firstModal = true;
 
 
 async function initMap() {
@@ -24,16 +22,27 @@ async function initMap() {
       gestureHandling: "greedy"
     });
 
-
-    const mainModal = document.getElementById("modal");
+    const modalButton = document.getElementById("modal-button-container")
+    const mainModal = document.getElementById("modal")
     const startButton = document.getElementById("start")
+    modalButton.addEventListener("click", () => {
+        if (!firstModal) {
+           startButton.innerHTML = "Continue Game" 
+        }
+        mainModal.style.display = "flex"
+        endGameModal.style.display = "none"
+    })
     startButton.addEventListener("click", () => {
         mainModal.style.display = "none";
+        firstModal = false;
     })
     const helpButton = document.getElementById("finish-game");
     const endGameModal = document.getElementById("end-game-modal");
     const forfeitButton = document.getElementById("forfeit");
     const closeBox = document.getElementById("close-end-game");
+    const finishedGame = document.getElementById("finished-game-modal")
+    const restartButton = document.getElementById("restart-button")
+    const finalScore = document.getElementById("final-score")
     helpButton.addEventListener("click", () => {
         endGameModal.style.display = "flex"
     })
@@ -42,6 +51,11 @@ async function initMap() {
     })
     forfeitButton.addEventListener("click", () => {
         mainModal.style.display = "none"
+        finishGame()
+        finishedGame.style.display = "flex"
+    })
+    restartButton.addEventListener("click", () => {
+        restartGame()
     })
     
 
@@ -104,18 +118,52 @@ async function initMap() {
             })
         })
     }
-  
-    function mapLocations(locationsArray) {      
+
+    function updateScore(currentScore) {
+        const score = document.getElementById("score");
+        score.innerHTML = "Current Score: " + currentScore
+    }
+
+    function mapLocations(locationsArray) {
+        locationsArray.forEach((location) => {
+            const marker = new google.maps.Marker({
+                position: {lat:location.location.lat, lng:location.location.lng},
+                map: gMap,
+                title: location.title,
+                icon: location.iconImage,
+                content: location.content
+            })
+            const infoWindow = new google.maps.InfoWindow({
+                content: marker.content
+            });
+            marker.addListener('click', () => {
+                infoWindow.open({
+                    anchor: marker,
+                    map: gMap
+                })
+            })
+        });
+    }
+
+    function finishGame() {
+        mapLocations(locationsArray);
+    }
+
+    function restartGame () {
+        location.reload()
+    }
+    
+    function playGame(locationsArray) {      
         let foundAllLocations = false;
         let currentLocationIndex = 0
         let locationFound = false
-        let isChangingCenter = false;
+        let isChangingCenter = false
+        let score = 0
         google.maps.event.addListener(gMap, 'center_changed', function () {
             if (isChangingCenter) {
                 return
             }
             isChangingCenter = true
-            updateGame();
             const mapCenter = gMap.getCenter();
             let newLng = mapCenter.lng();
             if (newLng > 180) {
@@ -141,9 +189,10 @@ async function initMap() {
             locationFound = isMarkerChoiceValid(marker, locationsArray[currentLocationIndex].location.lat, locationsArray[currentLocationIndex].location.lng)
             if (currentLocationIndex >= 9) {
                 createLocationMarker(locationsArray[currentLocationIndex])
-                foundAllLocations = true
-                console.log("Congrats you finished the Game!")
-                return foundAllLocations
+                score += 10
+                updateScore(score)
+                finishedGame.style.display = "flex"
+                finalScore.innerHTML = "Congratulations! You finished the game with a score of: " + score
             }
             else if (locationFound && currentLocationIndex <= 9) {
                 createLocationMarker(locationsArray[currentLocationIndex])
@@ -152,26 +201,20 @@ async function initMap() {
                 setTimeout(() => {
                     locationFoundModal.style.display = "none";
                 }, 3000)
-                console.log(currentLocationIndex)
+                score += 10
+                updateScore(score)
                 currentLocationIndex += 1
             } else {
-                console.log("keep trying")
+                score -= 5
+                if (score < 0) {
+                    score = 0
+                }
+                updateScore(score);
             }
         })
     }
 
-    function updateGame() {
-        //console.log('Function updateGame()');
-        const zoomLevel = gMap.getZoom();
-        var inBounds = false;
-
-        if(gMap.getBounds().contains({lat: 41.5250, lng:-88.0817})) {
-            inBounds = true;
-        }
-        console.log("inbounds:" + inBounds + " zoomLevel: "+zoomLevel);
-    }
-    mapLocations(locationsArray);
-    // addMarker();
+    playGame(locationsArray);
   }
   
 initMap();
